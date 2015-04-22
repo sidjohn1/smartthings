24241d1bbb4dc4271347d2415e38495983fc54b6
Sidney Johnson <sidjohn1@gmail.com>
2015-04-22 15:44:36 -0400

    Update ThinkingCleanerer.groovy

--- ThinkingCleanerer.groovy
+++ ThinkingCleanerer.groovy
@@ -2,6 +2,7 @@
  *  Thinking Cleanerer
  *  Smartthings SmartApp
  *  Copyright 2014 Sidney Johnson
+ *  If you like this app, please support the developer via PayPal: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XKDRYZ3RUNR9Y
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,33 +13,42 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *	Version: 1.0 - Initial Version
+ *	Version: 1.2 - Added error push notifcation, and better icons
  */
+ 
 definition(
     name: "Thinking Cleanerer",
     namespace: "sidjohn1",
     author: "Sidney Johnson",
-    description: "Handles polling and job notification for Thinking Cleaner devicetype",
+    description: "Handles polling and job notification for Thinking Cleaner",
     category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "http://cdn.device-icons.smartthings.com/Appliances/appliances13-icn.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Appliances/appliances13-icn@2x.png",
+    iconX3Url: "http://cdn.device-icons.smartthings.com/Appliances/appliances13-icn@3x.png")
 
 
 preferences {
-    section("Monitor This Roomba..."){
-		input "switch1", "capability.switch", multiple: false
+    section("Select a Roomba to monitor..."){
+		input "switch1", "device.ThinkingCleaner", title: "Monitored Roomba", required: true, multiple: false
 
+    }
+        section("Select events to be notified of..."){
+			input "sendRoombaOn", "enum", title: "Roomba On?", options: ["Yes", "No"], required: false, defaultValue: "No"
+            input "sendRoombaOff", "enum", title: "Roomba Off?", options: ["Yes", "No"], required: false, defaultValue: "No"
+            input "sendRoombaError", "enum", title: "Roomba Error?", options: ["Yes", "No"], required: false, defaultValue: "No"
     }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-
+	sendEvent(descriptionText:"Installed with settings: ${settings}", eventType:"SOLUTION_EVENT", displayed: true)
+    log.trace "Installed with settings: ${settings}"
 	initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+	sendEvent(descriptionText:"Updated with settings: ${settings}", eventType:"SOLUTION_EVENT", displayed: true)
+    log.trace "Updated with settings: ${settings}"
     unsubscribe()
     unschedule("poll")
 	initialize()
@@ -47,17 +57,42 @@ def updated() {
 def initialize() {
 	subscribe(switch1, "switch.on", eventHandler)
 	subscribe(switch1, "switch.off", eventHandler)
+    subscribe(switch1, "status.error", eventHandler)
 }
 
 def eventHandler(evt) {
-	if (evt.value == "on") {
-    	unschedule("poll")
-        schedule("0 0/1 * * * ?", "poll")
-	}
-	else {
-		unschedule("poll")
-        schedule("0 0/30 * * * ?", "poll")
-	}
+	unschedule("poll")
+    switch (evt.value) {
+    	case "error":
+            sendEvent(descriptionText:"${switch1.displayName} has an error, polling every hour", eventType:"SOLUTION_EVENT", displayed: true)
+        	log.trace "${switch1.displayName} has an error, polling every hour"
+        	def msg = "${switch1.displayName} has an error"
+			schedule("0 0/60 * * * ?", "poll")
+            if (sendRoombaError == "Yes") {
+			sendPush(msg)
+			}
+		break;
+        
+		case "on":
+        	sendEvent(descriptionText:"${switch1.displayName} is on, polling every minute", eventType:"SOLUTION_EVENT", displayed: true)
+        	log.trace "${switch1.displayName} is on, polling every minute"
+            def msg = "${switch1.displayName} is on"
+        	schedule("0 0/1 * * * ?", "poll")
+			if (sendRoombaOn == "Yes") {
+			sendPush(msg)
+			}
+		break;
+        
+		default:
+        	sendEvent(descriptionText:"${switch1.displayName} is off, polling every hour", eventType:"SOLUTION_EVENT", displayed: true)
+        	log.trace "${switch1.displayName} is off, polling every hour"
+			def msg = "${switch1.displayName} is off"
+        	schedule("0 0/60 * * * ?", "poll")
+			if (sendRoombaOff == "Yes") {
+				sendPush(msg)
+			}
+		break;
+    }
 }
 
 def poll() {
